b3ee09bfa4568539ea7990477ae4dfeb1ca8a5e3
AustinWells <austin.wells@mail.utoronto.ca>
2017-12-07 14:57:57 -0500

    added header file for stacks

---
+++ stacks/stack.h
@@ -0,0 +1,15 @@
+typedef struct Stack {
+  int top;
+  int *data;
+  size_t capacity;
+
+} Stack;
+
+int init_stack(Stack *s);
+int size(Stack *s);
+int Stack_empty(Stack *s);
+int Stack_full(Stack *s);
+int Stack_push(Stack *s, int new);
+int Stack_pop(Stack *s);
+int Stack_peek(Stack *s);
+void Stack_print(Stack *s);
